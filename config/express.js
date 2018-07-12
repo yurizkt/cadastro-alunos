a3172b8e0ce7858f419579e687304f1e677f5490
Yuri Zicatti <yuri.zkt@gmail.com>
2018-07-12 15:05:11 -0300

    Utilizei middlewares próprios para imprementar o tratamento de paginas de erro e de paginas nao encontradas

--- config/express.js
+++ config/express.js
@@ -19,5 +19,18 @@ module.exports = function(){
 		.then('infra')
 		.into(app)
 
+	app.use(function(req, res, next){
+		res.status(404).render('erros/404')
+		next()
+	})
+
+	app.use(function(error, req, res, next){
+		if(process.env.NODE_ENV == 'production'){
+			res.status(500).render('erros/500')
+			return
+		}
+		next(error)
+	})
+
 	return app
 }
